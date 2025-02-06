02ea9d2073b2600ccc585ce9fe93a095fa17c900
Galileu Santos <galilasmb@gmail.com>
2025-02-06 14:31:26 -0300

    adjusting reconstruir dados

--- scripts/reconstruir_dados_setores.sh
+++ scripts/reconstruir_dados_setores.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Nome do arquivo final
-OUTPUT_FILE="shapes/setores_light.gpkg"
+OUTPUT_FILE="shapes/setores_ligth_processado.gpkg"
 
 # Verifica se há arquivos divididos
-PARTS=$(ls shapes/setores_part_* 2>/dev/null)
+PARTS=$(ls shapes/setores_ligth_processado_part_* 2>/dev/null)
 
 if [ -z "$PARTS" ]; then
     echo "Nenhum arquivo de partes encontrado em ./shapes/"
@@ -13,7 +13,7 @@ fi
 
 # Junta os arquivos
 echo "Unindo arquivos em $OUTPUT_FILE..."
-cat shapes/setores_part_* > "$OUTPUT_FILE"
+cat shapes/setores_ligth_processado_part_* > "$OUTPUT_FILE"
 
 # Confirmação
 echo "Arquivo reconstruído com sucesso: $OUTPUT_FILE"
